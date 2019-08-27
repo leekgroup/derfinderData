5efbdeb378a40137a3e0415d0d376fb3c60f3190
Leonardo Collado Torres <lcolladotor@gmail.com>
2019-08-27 11:19:09 -0400

    v2.3.2 -- update docs
    * Added `Encoding: UTF-8`
    * Ran `usethis::use_roxygen_md()` + `roxygen2md::roxygen2md()` + `devtools::document()`
    * Added an RStudio project file

--- R/derfinderData-package.R
+++ R/derfinderData-package.R
@@ -1,13 +1,14 @@
 #' Processed BigWigs from BrainSpan for examples
 #'
-#' Processed 22 samples from BrainSpan keeping only the information for 
-#' chromosome 21. Data is stored in the BigWig format and is used for examples #' in other packages.
+#' Processed 22 samples from BrainSpan keeping only the information for
+#' chromosome 21. Data is stored in the BigWig format and is used for examples
+#' in other packages.
 #'
 #' @name derfinderHelper-package
 #' @aliases derfinderHelper-package
 #' @docType package
-#' @author Leonardo Collado-Torres <lcollado@@jhu.edu>
+#' @author Leonardo Collado-Torres <lcolladotor@@gmail.com>
 #'
 #'
 #' @keywords package
-NULL 
+NULL
